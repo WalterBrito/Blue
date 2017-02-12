943452ae62979f23aa35a35b3a5eecc119da9289
WalterBrito <waltter.britto@gmail.com>
2017-02-12 17:07:43 -0300

    Render the views

--- app/views/main_pages/_main_page.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! main_page, :id, :title, :post, :created_at, :updated_at
-json.url main_page_url(main_page, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   resources :line_items
   resources :carts
-  #get 'deals/index'
   resources :contacts
   resources :stores
-  resources :main_pages
   resources :products
   resources :deals
+  #get 'deals/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'main_pages#index'
+  root 'deals#index'
 end

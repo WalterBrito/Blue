db53e7fe3741690c3bec88b07e1ff71ac8e7ba58
WalterBrito <waltter.britto@gmail.com>
2017-02-13 17:04:12 -0300

    adding products to cart

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170212135109) do
+ActiveRecord::Schema.define(version: 20170213184351) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,8 +31,9 @@ ActiveRecord::Schema.define(version: 20170212135109) do
   create_table "line_items", force: :cascade do |t|
     t.integer  "product_id"
     t.integer  "cart_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.integer  "quantity",   default: 1
     t.index ["cart_id"], name: "index_line_items_on_cart_id", using: :btree
     t.index ["product_id"], name: "index_line_items_on_product_id", using: :btree
   end
